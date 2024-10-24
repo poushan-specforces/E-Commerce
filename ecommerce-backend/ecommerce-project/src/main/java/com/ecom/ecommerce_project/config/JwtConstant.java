bf95bcb6cf8b38d099ba6443d1c0f3a1b24fba59
Poushan <poushan.shan@gmail.com>
2024-10-25 00:44:33 +0530

    Create backend

---
+++ ecommerce-backend/ecommerce-project/src/main/java/com/ecom/ecommerce_project/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.ecom.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "hgdityaeyitevlsnvvnrtdusnv";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ ecommerce-backend/ecommerce-project/src/main/java/com/ecom/ecommerce_project/model/User.java
@@ -0,0 +1,19 @@
+package com.ecom.ecommerce_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private String role;
+    private String mobile;
+}
